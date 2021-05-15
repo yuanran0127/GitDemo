23da34ec2ee18bc3b81c02ba8d318851ea3ab9b6
yuanran0127 <1826452653@qq.com>
2021-05-15 13:30:56 +0800

    Revert "尝试进行第三次更改"
    This reverts commit 6a2cd6f1b4353bbbe443c5a1dcfea5bdd25da06e.
    
    # Conflicts:
    #	demo-module-1/src/com/school/Test1.java

--- demo-module-1/src/com/school/Test1.java
+++ demo-module-1/src/com/school/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("hello2");
+
         System.out.println("hello3");
     }
 }
