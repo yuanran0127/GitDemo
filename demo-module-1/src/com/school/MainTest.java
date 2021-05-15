145e2ac1c47b151ace362cae05f59047b12f8276
yuanran0127 <1826452653@qq.com>
2021-05-15 14:15:57 +0800

    Merge branch 'test'
    # Conflicts:
    #	demo-module-1/src/com/school/MainTest.java

--- demo-module-1/src/com/school/MainTest.java
+++ demo-module-1/src/com/school/MainTest.java
@@ -5,5 +5,7 @@ public class MainTest{
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello master");
+
+        System.out.println("hello test");
     }
 }
